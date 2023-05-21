b0da7e41d3664ceb6ec77593fe5c2820035dac71
byMagg <byMag@hotmail.es>
2023-05-21 11:40:58 +0200

    update

--- src/app.test.js
+++ src/app.test.js
@@ -1,19 +1,22 @@
 const request = require("supertest");
 
 const app = require("./app");
+const mongoose = require('mongoose');
 
 describe("GET /", () => {
-    it('obtains token from spotify', (done) => {
-        request(app)
+    it('obtains token from Spotify', async () => {
+        const response = await request(app)
             .get('/api/generate_token')
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                expect(res.body).toHaveProperty('access_token');
-                expect(res.body).toHaveProperty('token_type');
-                expect(res.body).toHaveProperty('expires_in');
-                done();
-            });
+            .expect(200);
+
+        expect(response.body).toHaveProperty('access_token');
+        expect(response.body).toHaveProperty('token_type');
+        expect(response.body).toHaveProperty('expires_in');
     });
+
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
 });
